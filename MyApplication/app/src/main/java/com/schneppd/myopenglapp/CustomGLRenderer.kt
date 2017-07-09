1a316b3d214dcdbb1520dfe851d23897f3f65abb
david.schnepp <david.schnepp@schneppd.com>
2017-07-09 17:44:36 +0200

    Add component + Square model + GlSurfaceView

--- MyApplication/app/src/main/java/com/schneppd/myopenglapp/CustomGLRenderer.kt
+++ MyApplication/app/src/main/java/com/schneppd/myopenglapp/CustomGLRenderer.kt
@@ -50,14 +50,25 @@ class CustomGLRenderer : GLSurfaceView.Renderer{
 			var error = GLES20.glGetError()
 			while (error != GLES20.GL_NO_ERROR) {
 				Log.e(TAG, glOperation + ": glError " + error)
-				throw RuntimeException(glOperation + ": glError " + error)
+				//throw RuntimeException(glOperation + ": glError " + error)
 				error = GLES20.glGetError()
 			}
 		}
 	}
 
-	private var mTriangle:Triangle = Triangle()
-	private var mSquare:Square = Square()
+	var _angle = 0f
+	var angle:Float
+		get() = _angle
+		set(value) {
+			_angle = value
+		}
+
+	private val mTriangle:Triangle by lazy {
+		Triangle()
+	}
+	private val mSquare:Square by lazy {
+		Square()
+	}
 
 	// mMVPMatrix is an abbreviation for "Model View Projection Matrix"
 	private var mMVPMatrix:FloatArray = FloatArray(16)
@@ -65,19 +76,17 @@ class CustomGLRenderer : GLSurfaceView.Renderer{
 	private var mViewMatrix:FloatArray = FloatArray(16)
 	private var mRotationMatrix:FloatArray = FloatArray(16)
 
-	override fun onSurfaceCreated(p0: GL10?, p1: EGLConfig?) {
+	override fun onSurfaceCreated(p0: GL10, p1: EGLConfig?) {
 		// Set the background frame color
 		GLES20.glClearColor(0.0f, 0.0f, 0.0f, 1.0f)
-
 	}
 
-	override fun onDrawFrame(p0: GL10?) {
-		TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+	override fun onDrawFrame(p0: GL10) {
 		val scratch = FloatArray(16)
 		// Draw background color
-		GLES20.glClear(GLES20.GL_COLOR_BUFFER_BIT | GLES20.GL_DEPTH_BUFFER_BIT)
+		GLES20.glClear(GLES20.GL_COLOR_BUFFER_BIT or GLES20.GL_DEPTH_BUFFER_BIT)
 		// Set the camera position (View matrix)
-		Matrix.setLookAtM(mViewMatrix, 0, 0, 0f, -3f, 0f, 0f, 0f, 0f, 1.0f, 0.0f)
+		Matrix.setLookAtM(mViewMatrix, 0, 0f, 0f, -3f, 0f, 0f, 0f, 0f, 1.0f, 0.0f)
 		// Calculate the projection and view transformation
 		Matrix.multiplyMM(mMVPMatrix, 0, mProjectionMatrix, 0, mViewMatrix, 0)
 		// Draw square
@@ -100,7 +109,7 @@ class CustomGLRenderer : GLSurfaceView.Renderer{
 		mTriangle.draw(scratch)
 	}
 
-	override fun onSurfaceChanged(p0: GL10?, width: Int, height: Int) {
+	override fun onSurfaceChanged(p0: GL10, width: Int, height: Int) {
 		// Adjust the viewport based on geometry changes,
 		// such as screen rotation
 		GLES20.glViewport(0, 0, width, height)
@@ -111,11 +120,5 @@ class CustomGLRenderer : GLSurfaceView.Renderer{
 		// in the onDrawFrame() method
 		Matrix.frustumM(mProjectionMatrix, 0, -ratio, ratio, -1f, 1f, 3f, 7f)
 	}
-
-	var angle:Float
-		get() = angle
-		set(value) {
-			angle = value
-		}
 
 }
