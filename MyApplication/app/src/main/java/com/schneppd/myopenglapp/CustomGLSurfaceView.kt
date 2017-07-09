ec7c3859664e9761f2af1a1cc402f67ec5c7a1fc
david.schnepp <david.schnepp@schneppd.com>
2017-07-09 16:33:19 +0200

    Add component + triangle model + renderer

---
+++ MyApplication/app/src/main/java/com/schneppd/myopenglapp/CustomGLSurfaceView.kt
@@ -0,0 +1,10 @@
+package com.schneppd.myopenglapp
+
+import android.opengl.GLSurfaceView
+
+/**
+ * Created by schneppd on 7/9/17.
+ */
+class CustomGLSurfaceView : GLSurfaceView {
+
+}
---
+++ MyApplication/app/src/main/java/com/schneppd/myopenglapp/Square.kt
@@ -0,0 +1,7 @@
+package com.schneppd.myopenglapp
+
+/**
+ * Created by schneppd on 7/9/17.
+ */
+class Square {
+}
